56da0167ffeb94131350d8244392e899479b2a82
Mohsin0899 <mohsin.saifi98@gmail.com>
2022-03-01 16:15:24 +0530

    Final Commit

--- app/src/main/java/com/ali/convertx/MainActivity.java
+++ app/src/main/java/com/ali/convertx/MainActivity.java
@@ -5,17 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public void btnClick(View view){
         EditText dollarAmount = (EditText) findViewById(R.id.dollarAmount);
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
         String dollars = dollarAmount.getText().toString();
         Double doubleDollars = Double.parseDouble(dollars);
         Double doubleInr = 74.70 * doubleDollars;
-        String toastText = "The corresponding amount in Rupees is = Rs. " + doubleInr.toString();
-
-        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
+        textView2.setText("The Amount in Rupees is Rs. " + doubleInr );
+//        String toastText = "The corresponding amount in Rupees is = Rs. " + doubleInr.toString();
+//        Toast.makeText(this, toastText, Toast.LENGTH_SHORT).show();
     }
 
 
